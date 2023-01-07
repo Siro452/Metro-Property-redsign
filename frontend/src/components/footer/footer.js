b84e86407a359cc1739c266c258036b54aa2d223
Nick Yeung <n.yeung91@gmail.com>
2023-01-08 00:30:34 +1300

    This bloody navbar lol

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,48 +1,69 @@
-import React from 'react'
+import React from "react";
 import styles from "./footer.module.css";
 
-import housePhoto from "../../assets/HouseTempPhoto.png"
-import facebook from "../../assets/facebooklogo.svg"
-import instagram from "../../assets/instagramlogo.svg"
-import linkedin from "../../assets/linkedinlogo.svg"
-import twitter from "../../assets/twitterlogo.svg"
+import housePhoto from "../../assets/HouseTempPhoto.png";
+import facebook from "../../assets/facebooklogo.svg";
+import instagram from "../../assets/instagramlogo.svg";
+import linkedin from "../../assets/linkedinlogo.svg";
+import twitter from "../../assets/twitterlogo.svg";
 
-import ContactUsRed from '../buttons/contactusred';
+import ContactUsRed from "../buttons/contactusred";
 
 export default function footer() {
   return (
     <div className={styles.outermostcontainer}>
-
-        <div className={styles.footercontent}>
-            <div className={styles.footerblurb}>Metro NZ Property Management has offices conveniently located in Central Auckland, but operates throughout New Zealand and also internationally.</div>
-
-            <div className={styles.iconsection}>
-                <div className={styles.logosection}>
-                    <img className={styles.metrologo} src="https://metronz.co.nz/wp-content/uploads/2022/09/header-logo.svg" alt="metrologo" />
-                    <p className={styles.address}>Level 33, ANZ Centre, 23-29 Albert St,<br /> Auckland 1010, New Zealand</p>
-                </div>
-                <div className={styles.socialssection}>
-                    <ContactUsRed />
-                    <div className={styles.socialmediaicons}>
-                        <a href="https://www.facebook.com/bangtan.official/"><div className={styles.socialmediaicon}><img src={facebook} alt="facebook icon" /></div></a>
-                        <a href="https://www.instagram.com/bts.bighitofficial/?hl=en"><div className={styles.socialmediaicon}><img src={instagram} alt="instagram icon" /></div></a>
-                        <div className={styles.socialmediaicon}><img src={linkedin} alt="linkedin icon" /></div>
-                        <a href="https://twitter.com/bts_twt"><div className={styles.socialmediaicon}><img src={twitter} alt="twitter icon" /></div></a>
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.contactdetails}>
-                <div>© 2022 Metro NZ Property Management | Disputes Process</div>
-                <div>09 391 4642 | +64 21 642 119 | info@metronz.co.nz</div>
-            </div>
+      <div className={styles.footercontent}>
+        <div className={styles.footerblurb}>
+          Metro NZ Property Management has offices conveniently located in
+          Central Auckland, but operates throughout New Zealand and also
+          internationally.
         </div>
-        
-        <img classname={styles.housephoto} src={housePhoto} alt="modern house" />
-        <div className={styles.photogradient}></div>
 
-       
-    
+        <div className={styles.iconsection}>
+          <div className={styles.logosection}>
+            <img
+              className={styles.metrologo}
+              src="https://metronz.co.nz/wp-content/uploads/2022/09/header-logo.svg"
+              alt="metrologo"
+            />
+            <p className={styles.address}>
+              Level 33, ANZ Centre, 23-29 Albert St,
+              <br /> Auckland 1010, New Zealand
+            </p>
+          </div>
+          <div className={styles.socialssection}>
+            <ContactUsRed />
+            <div className={styles.socialmediaicons}>
+              <a href="https://www.facebook.com/bangtan.official/">
+                <div className={styles.socialmediaicon}>
+                  <img src={facebook} alt="facebook icon" />
+                </div>
+              </a>
+              <a href="https://www.instagram.com/bts.bighitofficial/?hl=en">
+                <div className={styles.socialmediaicon}>
+                  <img src={instagram} alt="instagram icon" />
+                </div>
+              </a>
+              <div className={styles.socialmediaicon}>
+                <img src={linkedin} alt="linkedin icon" />
+              </div>
+              <a href="https://twitter.com/bts_twt">
+                <div className={styles.socialmediaicon}>
+                  <img src={twitter} alt="twitter icon" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.contactdetails}>
+          <div>© 2022 Metro NZ Property Management | Disputes Process</div>
+          <div>09 391 4642 | +64 21 642 119 | info@metronz.co.nz</div>
+        </div>
+      </div>
+
+      <img classname={styles.housephoto} src={housePhoto} alt="modern house" />
+      <div className={styles.photogradient}></div>
     </div>
-  )
+  );
 }
--- frontend/src/pages/homepage/homepage.js
+++ frontend/src/pages/homepage/homepage.js
@@ -1,9 +1,11 @@
 import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
 
 export default function HomePage() {
   return (
     <div>
       <NavBar></NavBar>
+      <Footer></Footer>
     </div>
   );
 }

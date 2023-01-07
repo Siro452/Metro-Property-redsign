3d54a9fe567b1b51d358937a34eb94b671d16b42
Nick Yeung <n.yeung91@gmail.com>
2023-01-08 01:24:21 +1300

    Homepage header finished

--- frontend/src/components/buttons/searchbuttonred.js
+++ frontend/src/components/buttons/searchbuttonred.js
@@ -1,9 +1,9 @@
 import styles from "./buttonstyles.module.css";
 
-export default function SearchButtonRed() {
+export default function SearchButtonRed(props) {
   return (
     <div>
-      <button className={styles.searchbuttonred}></button>
+      <button className={styles.searchbuttonred}>{props.buttonTitle}</button>
     </div>
   );
 }
--- frontend/src/pages/homepage/homepage.js
+++ frontend/src/pages/homepage/homepage.js
@@ -1,10 +1,12 @@
 import NavBar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
+import HomePageBodyHeader from "./sections/bodyheader/homepagebodyheader";
 
 export default function HomePage() {
   return (
     <div>
       <NavBar></NavBar>
+      <HomePageBodyHeader />
       <Footer></Footer>
     </div>
   );

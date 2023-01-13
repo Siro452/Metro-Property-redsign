cf78885f9cb2890a9950c7027510abe15628796a
Nick Yeung <n.yeung91@gmail.com>
2023-01-14 02:26:53 +1300

    small minor change

--- backend/rental.js
+++ backend/rental.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 
 const rentalSchema = new mongoose.Schema({
-    title: String,
-    image: {
-        featuredimg: String,
-        galleryimg: String,
-    },
-    address: {
-        streetaddress: String,
-        city: String,
-        district: String,
-        suburb: String,
-        postcode: Number,
-    },
-    featuredinfo: {
-        bedrooms: Number,
-        bathrooms: Number,
-        carparks: Number,
-        petfriendly: Boolean, 
-        furnished: Boolean,
-        publictransport: Number,
-        cyclepath: Number,
-        supermarket: Number, 
-        gym: Number,
-        park: Number,
-        featuredproperty: Boolean,
-    },
-    propertyinfo: String,
-    cost: Number
-})
+  title: String,
+  image: {
+    featuredimg: String,
+    galleryimg: [String],
+  },
+  address: {
+    streetaddress: String,
+    city: String,
+    district: String,
+    suburb: String,
+    postcode: Number,
+  },
+  featuredinfo: {
+    bedrooms: Number,
+    bathrooms: Number,
+    carparks: Number,
+    petfriendly: Boolean,
+    furnished: Boolean,
+    publictransport: Number,
+    cyclepath: Number,
+    supermarket: Number,
+    gym: Number,
+    park: Number,
+    featuredproperty: Boolean,
+  },
+  propertyinfo: String,
+  cost: Number,
+});
 
-module.exports = mongoose.model("Rental", rentalSchema)
+module.exports = mongoose.model("Rental", rentalSchema);

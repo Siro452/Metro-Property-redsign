dec053f532d9e569d6d2d21f943de65fb4bb7f1d
Bonnie Wall <bm-wall@outlook.com>
2023-01-17 00:08:11 +1300

    sorted basic structure and components for individual listings page

---
+++ frontend/src/pages/individuallisting/similarproperties.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./similarproperties.module.css";
+
+
+export default function SimilarProperties() {
+  return (
+    <div className={styles.outermostcontainer}>
+        <div className={styles.similarpropertiestitle}>Similar Properties</div>
+        <p>
+            We place high importance to finding a property for you that you will feel fully at home in and that you will want to stay in for a long time.
+        </p>
+
+        <div className={styles.cardcontainer}>
+            
+        </div>
+
+
+    </div>
+  )
+}
